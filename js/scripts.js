b39b5d66eab048f26d0d3a0746a1655c01b71085
Maldon <maldonmeehan@gmail.com>
2016-06-23 08:02:33 -0700

    change js to front end and back end, still not working

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,33 @@
-var answer = "";
-
-$("#play").click(function(event) {
-  event.preventDefault();
-  // $("ul#pingpong" li).remove();
-  var inputNumber = parseInt($("#yourNumber").val());
-  for (i=1; i<=inputNumber; i++)
-  {
+// business logic
+var pingPong = function pingPong(inputNumber){
+  var answer = [];
+  for (i=1; i<=inputNumber; i++){
     if (i%15===0) {
-      answer = "ping-pong";
+      answer.push ("ping-pong");
     } else if (i%5===0) {
-      answer = "pong";
+      answer.push ("pong");
     } else if (i%3===0) {
-      answer = "ping";
+      answer.push ("ping");
     } else {
-      answer = i;
+      answer.push (i);
     }
-    $("#pingpong").append("<li>" + answer + "</li>");
-  }
+  };
+  return answer;
+};
+
+// front end logic
+$(document).ready(function() {
+  $("#play").click(function(event) {
+    event.preventDefault();
+    $(".output-answer li").remove();
+    var inputNumber = parseInt($("input#input-number").val());
+    var pongResults = pingPong(inputNumber);
+    pongAnswer.forEach(function(answer) {
+      $(".output-answer").append("<li>" + answer + "</li>");
+      });
+    $("#answer").show();
+  });
 });
+
+
+  // $("ul#pingpong" li).remove();
